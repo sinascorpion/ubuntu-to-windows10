b8094132d6c7ded6e4ad7e8f46fad0a917f10eed
sinascorpion <s.scorpion2020@gmail.com>
2022-02-21 12:32:29 +0100

    Update ubuntu-to-windows10.sh

--- ubuntu-to-windows10.sh
+++ ubuntu-to-windows10.sh
@@ -32,13 +32,13 @@ fi
 sudo ln -s /usr/bin/genisoimage /usr/bin/mkisofs
 # Downloading resources
 sudo mkdir /mediabots /floppy /virtio
-link1_status=$(curl -Is https://www.dropbox.com/s/ad5nk8rv6bzuv7n/Win10_20H2_v2_English_x64.iso?dl=1 | grep HTTP | cut -f2 -d" " | head -1)
-link2_status=$(curl -Is https://www.dropbox.com/s/ad5nk8rv6bzuv7n/Win10_20H2_v2_English_x64.iso?dl=1 | grep HTTP | cut -f2 -d" ")
+link1_status=$(curl -Is http://download.microsoft.com/download/6/2/A/62A76ABB-9990-4EFC-A4FE-C7D698DAEB96/9600.17050.WINBLUE_REFRESH.140317-1640_X64FRE_SERVER_EVAL_EN-US-IR3_SSS_X64FREE_EN-US_DV9.ISO | grep HTTP | cut -f2 -d" " | head -1)
+link2_status=$(curl -Is https://ia601506.us.archive.org/4/items/WS2012R2/WS2012R2.ISO | grep HTTP | cut -f2 -d" ")
 #sudo wget -P /mediabots https://archive.org/download/WS2012R2/WS2012R2.ISO # Windows Server 2012 R2 
 if [ $link1_status = "200" ] ; then 
-	sudo wget -O /mediabots/Win10_20H2_v2_English_x64.iso https://www.dropbox.com/s/ad5nk8rv6bzuv7n/Win10_20H2_v2_English_x64.iso?dl=1 
+	sudo wget -O /mediabots/WS2012R2.ISO https://www.dropbox.com/s/cf9joyxu3ygw3fj/en-us_windows_11_business_editions_updated_jan_2022_x64_UNLOCKED_Softgozar.com.iso?dl=1
 elif [ $link2_status = "200" -o $link2_status = "301" -o $link2_status = "302" ] ; then 
-	sudo wget -P /mediabots https://www.dropbox.com/s/ad5nk8rv6bzuv7n/Win10_20H2_v2_English_x64.iso?dl=1
+	sudo wget -P /mediabots https://www.dropbox.com/s/cf9joyxu3ygw3fj/en-us_windows_11_business_editions_updated_jan_2022_x64_UNLOCKED_Softgozar.com.iso?dl=1
 else
 	echo -e "${RED}[Error]${NC} ${YELLOW}Sorry! None of Windows OS image urls are available , please report about this issue on Github page : ${NC}https://github.com/mediabots/Linux-to-Windows-with-QEMU"
 	echo "Exiting.."
